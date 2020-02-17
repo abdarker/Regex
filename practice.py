de7f53a2517c74bd1f8b5f93838fffca4820ebbb
Ab <abdarker@outlook.com>
2020-02-17 22:33:43 +0600

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,15 @@
+import re 
+
+pattern = re.compile(r'\d\d\d.\d\d\d.\d\d\d\d')
+with open('data.txt', 'r', encoding='utf-8') as f:
+    contents = f.read()
+
+    matches = pattern.finditer(contents) # will return multiple we can use sub()
+   # matches = pattern.findall(contents) # only gruops/ multiple group in tuples
+   # matches = pattern.match(contents) #at the beginning / no need for loop  can direct print  print(matches)
+   # matches = pattern.search(contents) # no need for loop, can direct print  print(matches)
+
+
+
+    for match in matches:
+        print(match)
---
+++ practice2.py
@@ -0,0 +1,16 @@
+import re
+
+urls = '''
+https://www.google.com
+http://coreyms.com
+https://youtube.com
+https://www.nasa.gov
+'''
+
+pattern = re.compile(r'https?://(www\.)?(\w+)(\.\w+)')
+
+#pattern2= re.compile(r'start', re.IGNORECASE)
+
+subbed_urls = pattern.sub(r'\2\3', urls)
+
+print(subbed_urls)
